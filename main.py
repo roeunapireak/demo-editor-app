04cd424ec972f9aefa9e76b64f834d3cb17efcbc
RoeunApireak <apireak.roeun.datau@gmail.com>
2024-10-18 20:11:10 +0700

    commit main.py

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from pygame import *
 
-from spriteClass import GrameSprite
+from spriteClass import GameSprite
 
+''' colors '''
+background = (200, 255, 255)
+
+window = display.set_mode((700, 500))
+window.fill(background)
+
+clock = time.Clock()
+
+''' variables '''
+game = True
+
+'''' game loop '''
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    display.update()
+    clock.tick(60)
